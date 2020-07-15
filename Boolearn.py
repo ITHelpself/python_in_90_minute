1c6a433b6aa447bb5ed014cc001e2b035c7b71a3
phithai <phithai289@gmail.com>
2020-07-16 02:33:07 +0700

    # operator boolean

--- Boolearn.py
+++ Boolearn.py
@@ -1,2 +1,11 @@
 boolValue = True
 print(boolValue)
+greaterThan = 7> 6
+print(greaterThan)
+lessthan = 7<6
+print(lessthan)
+equalTo = 7 == 6
+equalToPart2 = 7==7
+notEqualTo = 7!=6
+gEq = 7>=6
+lEq = 7<=6
