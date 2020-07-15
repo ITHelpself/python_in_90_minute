a2d8d5aa3b1b54eb9fd59f27dd84ce8bfcf9302d
phithai <phithai289@gmail.com>
2020-07-16 01:15:27 +0700

    #multiple string

--- string.py
+++ string.py
@@ -3,3 +3,6 @@ firstString = "Hello"
 secondString = "World"
 additionString = firstString + secondString
 print(additionString)
+# repeat string
+multipleString = firstString * 3
+print(multipleString)
