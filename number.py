f9fcbc3340eb79947cf11a2eaf78081bcd12407b
phithai <phithai289@gmail.com>
2020-07-16 01:41:21 +0700

    # +,-,*,/,%

--- number.py
+++ number.py
@@ -1,8 +1,11 @@
 # name = value
 number = 5
 print(number)
-# additional two number
+# additional two numbers
 intVariable = 7
 floatVariable = 6.3
 subtractionResult = intVariable - floatVariable
 print(subtractionResult)
+multipleResult = intVariable * floatVariable
+divisionResult = intVariable / floatVariable
+modulusResult = intvariable % 3
